67c0ce8a7df1cd5b5300698a6c2d3fe832dce792
Aarthi Sathya Narayanan <sathyanarayanan.aarthi@in.bosch.com>
2022-08-02 20:49:44 +0530

    replaced enter in read me

--- current_range_test.py
+++ current_range_test.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 import sys
-import mock
 from current_range import detect_and_display_current_readings
 
 STD_OUT = 'sys.stdout'
@@ -15,16 +14,7 @@ class CurrentRangeTest(unittest.TestCase):
           
   
 
-class Test(unittest.TestCase):
-  def test_infers_breach_as_per_limits(self):
-    with mock.patch.object(unittest, "main", return_value=42):
-      with mock.patch.object(unittest, "__name__", "__main__"):
-        with mock.patch.object(sys,'exit') as mock_exit:
-          init()         
-          assert mock_exit.call_args[0][0] == 42
-        
-def init():
-  if __name__ == '__main__':
-    sys.exit(unittest.main())
 
-init()
+        
+if __name__ == '__main__':
+  sys.exit(unittest.main())
